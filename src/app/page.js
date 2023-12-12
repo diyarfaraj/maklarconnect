4a662ae1d1571242a9ad6cfd74a9eef45d23cb3a
Diyar Faraj <diyar.faraj@gmail.com>
2023-12-12 01:19:02 +0100

    header fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Home_V1 from "./(home)/home-v1/page";
 import Wrapper from "./layout-wrapper/wrapper";
 
 export const metadata = {
-  title: "Home v1 || Homez - Real Estate NextJS Template",
+  title: "MäklarConnect - Från mäklare till mäklare",
 };
 
 export default function MainRoot() {
